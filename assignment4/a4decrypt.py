b8abe65ef32648fd10a17bcfa3ad3183286c3042
David Josephs <josephd@clarkson.edu>
2016-11-29 11:15:06 -0500

    updates

--- assignment4/a4decrypt.py
+++ assignment4/a4decrypt.py
@@ -2,11 +2,10 @@ import math
 import sys
 
 f = open('a4.cipher.txt')
-
-
 p = 12399363407314802534894337901181367912365390278953001273409754922038825316283282383095029550773749866734231576917322906183500383575332035336227711526925327
 A = 12079055304900077881496017093520705553618624248737774318027533001099811953633763825284638741718719990914320606147780023938639362506662800193686221296264344
-
+#p = 163
+#A = -22
 N = 3
 xp = 0
 yp = 1
@@ -14,42 +13,36 @@ values = [];
 values = list(f)
 
 cipher = []
-for x in values:
-    points = []
-    for j in str.split(x):
-        points.append(int(j))
-    cipher.append(points)
-    
-
-#first find full mask by F = N * H = 3H = 2H + H
-
-def full_mask(H):
-    #    return = point_double(point_double(H)) # since N = 4
-    return point_add(H, point_double(H)) # since N = 3
+mask = []
+for v in values:
+    x = v.split(' ')
+    cipher.append([long(x[0]),long(x[1])])
+    mask.append([long(x[2]),long(x[3])])
 
 def point_double(P):
-    m = (((3 * (P[xp]**2)) + A) * (inverse((2 * P[yp]) % p))) % p
-    x = (m**2 - (2 * P[xp])) % p
-    y = (P[yp] + m * (x - P[xp])) % p
+    m = (((3 * (P[xp]**2)) + A) * (inverse(pow((2 * P[yp]),1,p)))) % p
+    x = (pow(m,2) - (2 * P[xp])) % p
+    y = (P[yp] + (m * (x - P[xp]))) % p
     y = -y % p
-    return [int(x), int(y)]
+    return [long(x), long(y)]
     
 def point_add(P, Q):
     m = ((Q[yp]-P[yp]) * inverse((Q[xp]-P[xp]) % p)) % p
     x = (m**2 - (P[xp] + Q[xp])) % p
     y = (P[yp] + m * (x - P[xp])) % p
     y = -y % p
-    return [int(x), int(y)]
+    return [long(x), long(y)]
     
 def inverse(e):
+    one = p
+    two = e
+    q = long(math.floor(one/two))
+    r = one % two
     x1 = 1
     y1 = 0
     x2 = 0
     y2 = 1
-    one = p
-    two = e
-    q = math.floor(one/two)
-    r = one % two
+    
     while(two !=1):
         tmp1 = x2
         tmp2 = y2
@@ -59,25 +52,27 @@ def inverse(e):
         y1 = tmp2
         one = two
         two = r
-        q = one/two
+        q = long(math.floor(one/two))
         r = one % two
-    return int(y2 % p)
+    return long(y2 % p)
 
 def decrypt(C, F):
     F[yp] = -F[yp] % p
     return point_add(C, F)
 
 def full_decrypt(C, H):
-    
-#    F = point_double(point_double(H)) # since N = 4
     F = point_add(H, point_double(H)) # since N = 3
     return decrypt(C, F)
-    
-z = open('a4partdecrypt.txt', 'w')
-for first in cipher:
-    C = [first[0],first[1]]
-    H = [first[2],first[3]]
+
+z = open('a4decrypt.txt', 'w')
+text = ''
+for i in range(314):
+    C = cipher[i]
+    H = mask[i]
     M = full_decrypt(C,H)
-    z.write(str(M[0]) + ' ' + str(M[1]) + '\n')
+    text += chr(M[0]);
+
+print text
+z.write(text) 
 
     
